5a9a5ff2dc665a285b741730d372a844184fe017
jbkwizera <jeanbaptistekwi@gmail.com>
2023-03-15 01:33:13 -0700

    from 'struct' to 'class' - rewrite

---
+++ student-info.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include "grade.h"
+#include "student-info.h"
+
+using std::istream;     using std::vector;
+
+Student_info::Student_info(): midterm(0), final_exam(0) {}
+
+Student_info::Student_info(std::istream& in)
+{
+    read(in);
+}
+
+istream& Student_info::read(istream& in)
+{
+    in >> n >> midterm >> final_exam;
+    read_hw(in, homework);
+    return in;
+}
+
+double Student_info::grade() const
+{
+    return ::grade(midterm, final_exam, homework);
+}
+
+bool compare(const Student_info& x, const Student_info& y)
+{
+    return x.name() < y.name();
+}
+
+bool compare_grade(const Student_info& x, const Student_info& y)
+{
+    return x.grade() < y.grade();
+}
---
+++ student-info.h
@@ -0,0 +1,29 @@
+#ifndef GUARD_student_info_h
+#define GUARD_student_info_h
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Student_info {
+public:
+    Student_info();
+    Student_info(std::istream&);
+    double grade() const;
+    std::istream& read(std::istream&);
+    std::string name() const { return n; }
+    bool valid() const { return !homework.empty(); }
+
+private:
+    std::string n;
+    double midterm, final_exam;
+    std::vector<double> homework;
+};
+
+std::istream&
+    read_hw(std::istream& in, std::vector<double>& hw);
+
+bool compare(const Student_info&, const Student_info&);
+bool compare_grade(const Student_info&, const Student_info&);
+
+#endif
