1248e4d4f73a9cb6c2b741ba8922a4a21c3211fc
jbkwizera <jeanbaptistekwi@gmail.com>
2023-03-26 14:57:12 -0700

    same interface as student class

--- core-handle.h
+++ core-handle.h
@@ -22,13 +22,37 @@ public:
         throw std::runtime_error("uninitialized core");
     }
 
+    bool pass() const {
+        if (cp) return cp->pass();
+        throw std::runtime_error("uninitialized core");
+    }
+
     double grade() const {
         if (cp) return cp->grade();
         throw std::runtime_error("uninitialized core");
     }
 
+    double grade_mean() const {
+        if (cp) return cp->grade_mean();
+        throw std::runtime_error("uninitialized core");
+    }
+
+    double grade_optimistic_median() const {
+        if (cp) return cp->grade_optimistic_median();
+        throw std::runtime_error("uninitialized core");
+    }
+
+    bool did_all_homework() const {
+        if (cp) return cp->did_all_homework();
+        throw std::runtime_error("uninitialized core");
+    }
+
     static bool compare(const CoreHandle& lhs, const CoreHandle& rhs) {
         return lhs.name() < rhs.name();
+    }
+
+    static bool compare_grade(const CoreHandle& lhs, const CoreHandle& rhs) {
+        return lhs.grade() < rhs.grade();
     }
 
 private:
