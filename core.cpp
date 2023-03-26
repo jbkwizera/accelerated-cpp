faf0d5ff6bdba5fd96991c352e445801efc7cbe5
jbkwizera <jeanbaptistekwi@gmail.com>
2023-03-25 18:33:26 -0700

    diving into oop - start w/ inheritance

---
+++ core.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "student-info.h"
+#include "core.h"
+
+using std::string;      using std::vector;
+using std::min;         using std::istream;
+
+/*** core implementation ***/
+string Core::name() const
+{
+    return n;
+}
+
+double Core::grade() const
+{
+    return ::grade(midterm, final_exam, homework);
+}
+
+istream& Core::read_common(istream& in)
+{
+    in >> n >> midterm >> final_exam;
+    return in;
+}
+
+istream& Core::read(istream& in)
+{
+    read_common(in);
+    ::read_hw(in, homework);
+    return in;
+}
+
+/*** Grad implementation ***/
+istream& Grad::read(istream& in)
+{
+    read_common(in);
+    in >> thesis;
+    ::read_hw(in, homework);
+    return in;
+}
+
+double Grad::grade() const
+{
+    return min(Core::grade(), thesis);
+}
+
+bool compare(const Core& lhs, const Core& rhs)
+{
+    return lhs.name() < rhs.name();
+}
---
+++ core.h
@@ -0,0 +1,38 @@
+#ifndef GUARD_core_h
+#define GUARD_core_h
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Core {
+public:
+    Core(): midterm(0), final_exam(0) {}
+    Core(std::istream& in) { read(in); }
+    std::istream& read(std::istream&);
+    std::string name() const;
+    double grade() const;
+
+protected:
+    double midterm, final_exam;
+    std::vector<double> homework;
+    std::istream& read_common(std::istream&);
+
+private:
+    std::string n;
+};
+
+class Grad: public Core {
+public:
+    Grad(): thesis(0) {};
+    Grad(std::istream& in) { read(in); }
+    double grade() const;
+    std::istream& read(std::istream&);
+
+private:
+    double thesis;
+};
+
+bool compare(const Core&, const Core&);
+
+#endif
